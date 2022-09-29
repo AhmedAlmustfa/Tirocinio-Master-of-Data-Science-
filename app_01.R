297a5839768c1ea480870d63f5768c279a99958d
AhmedAlmustafa <ahmedkhatir22@gmail.com>
2022-09-29 10:50:39 +0200

    first commit

--- app_01.R
+++ app_01.R
@@ -31,37 +31,52 @@ df <- read.csv("australian.csv")
   ui <- fluidPage(theme = shinytheme("cerulean"),
     navbarPage(
       # theme = "cerulean",  # <--- To use a theme, uncomment this
-      "My first app",
+      "App",
       tabPanel("Random Forest Model",
         sidebarLayout(
                sidebarPanel(
         h2("Installation"),
-        p("Shiny is available on CRAN, so you can install it in the usual way from your R console:"),
-        code('install.packages("shiny")'),
+        p("Random forest is available on CRAN, so you can install it in the usual way from your R console:"),
+        code('install.packages("randomForest")'),
         br(),
         br(),
         br(),
         br(),
-        img(src = "rstudio.png", height = 70, width = 200),
+        img(src = "forest.png", height = 70, width = 200),
         br(),
         "Shiny is a product of ", 
         span("RStudio", style = "color:blue")
       ),
       mainPanel(
-        h1("Introducing Shiny"),
-        p("Shiny is a new package from RStudio that makes it ", 
-          em("incredibly easy "), 
-          "to build interactive web applications with R."),
+        h1("Introducing Random Forest"),
+        p("Random forest, like its name implies, consists of a large number of individual
+          decision trees that operate as an", 
+          em("ensemble. "), 
+          "Each individual tree in the random forest spits out a class prediction and the
+          class with the most votes becomes our model’s prediction."),
         br(),
         p("For an introduction and live examples, visit the ",
-          a("Shiny homepage.", 
-            href = "http://shiny.rstudio.com")),
+          a("Towards Data Science.", 
+            href = "https://towardsdatascience.com/understanding-random-forest-58381e0602d2")),
         br(),
-        h2("Features"),
-        p("- Build useful web applications with only a few lines of code-no JavaScript required."),
-        p("- Shiny applications are automatically 'live' in the same way that ", 
-          strong("spreadsheets"),
-          " are live. Outputs change instantly as users modify inputs, without requiring a reload of the browser.")
+        h4("The reason that the random forest model works so well is:"),
+        p("- A large number of relatively uncorrelated models (trees) operating as 
+          a committee will outperform any of the individual constituent models."),
+        
+        p("The low correlation between models is the key Just like how investments 
+          with low correlations like stocks and bonds come together to form a portfolio 
+          that is greater than the sum of its parts, uncorrelated models can produce 
+          ensemble predictions that are more accurate than any of the individual predictions.", 
+          strong("The reason for this wonderful effect is that the trees protect each other from their 
+          individual errors"), "(as long as they don’t constantly all err in the same direction). 
+          While some trees may be wrong, many other trees will be right, so as a group the 
+          trees are able to move in the correct direction. So the prerequisites for random 
+          forest to perform well are: "),
+        
+        p("- There needs to be some actual signal in our features so that models built using 
+          hose features do better than random guessing."),
+        p("- The predictions (and therefore the errors) made by the individual trees need to 
+          have low correlations with each other.")
       )
       )),
       tabPanel("Visualization", 
@@ -190,7 +205,7 @@ df <- read.csv("australian.csv")
                  
                )
       )),
-      tabPanel("Downloading Data", "Blank",
+      tabPanel("Downloading Data", "Blank"
                
                    
                 )
